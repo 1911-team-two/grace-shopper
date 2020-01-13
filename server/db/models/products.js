e597dbd33abc46d5caec6c0e7952ee5e8380d404
Becca Duhamel <rduhamel9@gmail.com>
2020-01-13 16:17:38 -0500

    feature: added the users and products models

---
+++ server/db/models/products.js
@@ -0,0 +1,48 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://leeresidentialnyc.com/wp-content/uploads/2018/09/image-not-found-1.jpg'
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    isFloat: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  category: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  filter: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    validate: {
+      isIn: [['nature', 'art', 'funny']]
+    },
+    allowNull: false
+  }
+})
+
+module.exports = Product
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -26,6 +26,107 @@ const User = db.define('user', {
   },
   googleId: {
     type: Sequelize.STRING
+  },
+  fullName: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  streetBilling: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  cityBilling: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  stateBilling: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    validate: {
+      isIn: [
+        [
+          'AL',
+          'AK',
+          'AS',
+          'AZ',
+          'AR',
+          'CA',
+          'CO',
+          'CT',
+          'DE',
+          'DC',
+          'FM',
+          'FL',
+          'GA',
+          'GU',
+          'HI',
+          'ID',
+          'IL',
+          'IN',
+          'IA',
+          'KS',
+          'KY',
+          'LA',
+          'ME',
+          'MH',
+          'MD',
+          'MA',
+          'MI',
+          'MN',
+          'MS',
+          'MO',
+          'MT',
+          'NE',
+          'NV',
+          'NH',
+          'NJ',
+          'NM',
+          'NY',
+          'NC',
+          'ND',
+          'MP',
+          'OH',
+          'OK',
+          'OR',
+          'PW',
+          'PA',
+          'PR',
+          'RI',
+          'SC',
+          'SD',
+          'TN',
+          'TX',
+          'UT',
+          'VT',
+          'VI',
+          'VA',
+          'WA',
+          'WV',
+          'WI',
+          'WY'
+        ]
+      ]
+    }
+  },
+  zipBilling: {
+    type: Sequelize.NUMBER,
+    allowNull: true
+  },
+  streetShipping: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  cityShipping: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  stateShipping: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  zipShipping: {
+    type: Sequelize.NUMBER,
+    allowNull: true
   }
 })
 
