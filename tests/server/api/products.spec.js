c74d385987b2dfe175026c25c493283a45afe443
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-01-14 13:24:02 -0500

    initial commit

--- tests/server/api/products.spec.js
+++ tests/server/api/products.spec.js
@@ -2,8 +2,8 @@
 
 const {expect} = require('chai')
 const request = require('supertest')
-const db = require('../db')
-const app = require('../index')
+const db = require('../../../server/db')
+const app = require('../../../server/index')
 const Product = db.model('product')
 
 describe('Product routes', () => {
