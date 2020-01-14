5384dc933c012e83a311ea43152d8eec35449714
Mika Busante <mika@mikabusante.com>
2020-01-14 14:42:40 -0500

    Working on tests

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,34 +1,18 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+
+import Thumbnail from './Thumbnail'
 
 // NOTE: all css is temporary, will replace with better CSS after design is finalized
 
 const Home = () => {
   return (
     <div>
-      <h1>All products</h1>
+      <h2>All products</h2>
 
       <div style={{display: 'flex'}}>
-        <div>
-          <Link to="/product/1">
-            <img src="https://via.placeholder.com/250" alt="" />
-            <p>Placeholder Name</p>
-            <p>Artist Name</p>
-            <p>$00.00</p>
-          </Link>
-        </div>
-        <div>
-          <img src="https://via.placeholder.com/250" alt="" />
-          <p>Placeholder Name</p>
-          <p>Artist Name</p>
-          <p>$00.00</p>
-        </div>
-        <div>
-          <img src="https://via.placeholder.com/250" alt="" />
-          <p>Placeholder Name</p>
-          <p>Artist Name</p>
-          <p>$00.00</p>
-        </div>
+        <Thumbnail />
+        <Thumbnail />
+        <Thumbnail />
       </div>
     </div>
   )
