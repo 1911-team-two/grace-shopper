fc1f74784a7dd6c492c740d0ec954acabebbea4c
Mika Busante <mika@mikabusante.com>
2020-01-15 16:04:18 -0500

    Begin writing handle change function for checkout form

--- client/components/AddressForm.js
+++ client/components/AddressForm.js
@@ -1,35 +1,45 @@
 import React from 'react'
 
-export const AddressForm = props => {
-  return (
-    <div>
-      <h3>{props.title}</h3>
+export class AddressForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
 
-      <form>
-        {/* first name, last name, address 1, address 2, city, state , zip*/}
-        <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" />
+  render() {
+    return (
+      <div>
+        <h3>{this.props.title}</h3>
 
-        <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" />
+        <form>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            name={`firstName_${this.props.type}`}
+            onChange={this.props.handleChange}
+          />
 
-        <label htmlFor="addressLineOne">Address</label>
-        <input type="text" name="addressLineOne" />
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" name="lastName" />
 
-        <label htmlFor="addressLineTwo">Apartment (optional)</label>
-        <input type="text" name="addressLineTwo" />
+          <label htmlFor="addressLineOne">Address</label>
+          <input type="text" name="addressLineOne" />
 
-        <label htmlFor="city">City</label>
-        <input type="text" name="city" />
+          <label htmlFor="addressLineTwo">Apartment (optional)</label>
+          <input type="text" name="addressLineTwo" />
 
-        <label htmlFor="state">State</label>
-        <input type="text" name="state" />
+          <label htmlFor="city">City</label>
+          <input type="text" name="city" />
 
-        <label htmlFor="zip">Zip Code</label>
-        <input type="text" name="zip" />
-      </form>
-    </div>
-  )
+          <label htmlFor="state">State</label>
+          <input type="text" name="state" />
+
+          <label htmlFor="zip">Zip Code</label>
+          <input type="text" name="zip" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default AddressForm
--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,34 +1,56 @@
 import React from 'react'
 import AddressForm from './AddressForm'
 
-export const Checkout = () => {
-  return (
-    <div>
-      <h2>Checkout</h2>
-      <AddressForm title="Shipping Address" />
+export class Checkout extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
 
-      <div className="payment_wrapper">
-        <h3>Payment</h3>
-        <p>This is placeholder for payment details</p>
-        <label htmlFor="cc_number">Card Number</label>
-        <input type="text" name="cc_number" />
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-        <label htmlFor="cardname">Name on Card</label>
-        <input type="text" name="cardname" />
+  handleChange(e) {
+    console.log(e.target.name)
+    this.setState({
+      [e.target.name]: e.target.value
+    })
 
-        <label htmlFor="expiration">Expiration date (MM/YY)</label>
-        <input type="text" name="expiration" />
+    console.log(this.state)
+  }
 
-        <label htmlFor="security">Security Code</label>
-        <input type="text" name="security" />
+  render() {
+    return (
+      <div>
+        <h2>Checkout</h2>
+        <AddressForm
+          title="Shipping Address"
+          handleChange={this.handleChange}
+          type="shipping"
+        />
+
+        <div className="payment_wrapper">
+          <h3>Payment</h3>
+          <p>This is placeholder for payment details</p>
+          <label htmlFor="cc_number">Card Number</label>
+          <input type="text" name="cc_number" />
+
+          <label htmlFor="cardname">Name on Card</label>
+          <input type="text" name="cardname" />
+
+          <label htmlFor="expiration">Expiration date (MM/YY)</label>
+          <input type="text" name="expiration" />
+
+          <label htmlFor="security">Security Code</label>
+          <input type="text" name="security" />
+        </div>
+
+        <AddressForm title="Billing Address" />
+
+        {/* cart info here */}
+        {/* submit button here */}
       </div>
-
-      <AddressForm title="Billing Address" />
-
-      {/* cart info here */}
-      {/* submit button here */}
-    </div>
-  )
+    )
+  }
 }
 
 export default Checkout
