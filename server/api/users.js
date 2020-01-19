acbd16ba8f4a2a071ab921445b1d25dc6316c43e
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-01-19 15:16:29 -0500

    added routes to edit, and delete stuff

--- server/api/users.js
+++ server/api/users.js
@@ -4,23 +4,61 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
+    if (req.user && req.user.isAdmin) {
+      const users = await User.findAll({
+        // explicitly select only the id and email fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        attributes: ['id', 'email']
+      })
+      res.send(users)
+    } else {
+      res.sendStatus(401)
+    }
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/:userid', async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
-    const id = req.params.userid
-    const user = await User.findByPk(id)
-    res.json(user)
+    if (req.user && req.user.isAdmin) {
+      const id = req.params.userId
+      const user = await User.findByPk(id)
+      res.send(user)
+    } else {
+      res.sendStatus(401)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId
+    if (req.user) {
+      const user = await User.findByPk(userId)
+      await user.update(req.body)
+      res.send(user)
+    } else {
+      res.sendStatus(401)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId
+    if (req.user) {
+      let user = User.findByPk(userId)
+      await user.destroy()
+      res.sendStatus(204)
+    } else {
+      res.sendStatus(401)
+    }
   } catch (err) {
     next(err)
   }
